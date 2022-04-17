4e1d085ba7dfb0fe627270dcf224f7825be65b4e
ShotaKitazawa <skitazawa1121@gmail.com>
2022-04-17 22:18:29 +0900

    fix for tests

--- utils/metrics/reviewapp.go
+++ utils/metrics/reviewapp.go
@@ -10,24 +10,21 @@ const (
 )
 
 var (
-	UpVec                        *prometheus.GaugeVec
-	RequestToGitHubApiCounterVec *prometheus.CounterVec
-)
-
-func Register(registry prometheus.Registerer) {
 	UpVec = prometheus.NewGaugeVec(prometheus.GaugeOpts{
 		Namespace: metricsNamespace,
 		Subsystem: reviewappSubsystem,
 		Name:      "up",
 		Help:      "Operator's status is healthy when this flag equals 1",
 	}, []string{"name", "namespace", "appOrganization", "appRepository", "infraOrganization", "infraRepository"})
-	registry.MustRegister(UpVec)
-
 	RequestToGitHubApiCounterVec = prometheus.NewCounterVec(prometheus.CounterOpts{
 		Namespace: metricsNamespace,
 		Subsystem: reviewappSubsystem,
 		Name:      "",
 		Help:      "Operator's status is healthy when this flag equals 1",
 	}, []string{"name", "namespace", "kind"})
+)
+
+func Register(registry prometheus.Registerer) {
+	registry.MustRegister(UpVec)
 	registry.MustRegister(RequestToGitHubApiCounterVec)
 }
